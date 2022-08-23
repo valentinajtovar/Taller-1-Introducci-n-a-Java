69a26c459b225981ca76711b6c65592aaef26510
valentinajtovar <lv.jimenez@uniandes.edu.co>
2022-08-22 22:12:12 -0500

    commit

---
+++ src/Modificacion/Modification.java
@@ -0,0 +1,18 @@
+package Modificacion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import uniandes.dpoo.taller0.procesamiento.CalculadoraEstadisticas;
+import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+
+public class Modification {
+	
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		System.out.println("Hola, mundo!");
+		CalculadoraEstadisticas calc = LoaderOlimpicos.cargarArchivo("./data/atletas.csv");
+		System.out.println(calc.paisConMasMedallistas());
+		
+		
+	}
+}
